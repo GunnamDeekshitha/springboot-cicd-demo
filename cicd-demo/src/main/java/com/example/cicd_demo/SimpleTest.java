068b33818b6ae7f22747c759dec4515105374423
GunnamDeekshitha <deekshithagunnam@example.com>
2026-05-07 15:10:36 +0530

    Inittial CI/CD stage

---
+++ cicd-demo/src/main/java/com/example/cicd_demo/SimpleTest.java
@@ -0,0 +1,13 @@
+package com.example.cicddemo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class SimpleTest {
+
+    @Test
+    void testAddition() {
+        assertEquals(4, 2 + 2);
+    }
+}
---
+++ cicd-demo/src/main/java/com/example/cicd_demo/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.cicddemo.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return "CI/CD Working!";
+    }
+}
